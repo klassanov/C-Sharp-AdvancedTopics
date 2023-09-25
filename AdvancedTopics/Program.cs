0588bea6ee2410e746409f4aac47b815bfc1f2fe
Alexander Klassanov <klassanov@live.com>
2023-09-25 22:12:32 +0300

    Add project files.

---
+++ AdvancedTopics/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AdvancedTopics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            IntegralTypesOverflow();
+        }
+
+        static void IntegralTypesOverflow()
+        {
+            int a = int.MaxValue;
+            int b = 1;
+            //Overflow not handled: U get a garbage value
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
